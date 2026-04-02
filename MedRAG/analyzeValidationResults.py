c77cdd1020dc7520a3194d3095bdad08b8bf238b
tommasoBazzocchi869095 <102532068+tommasoBazzocchi869095@users.noreply.github.com>
2026-04-03 00:28:57 +0200

    Delete MedRAG directory

--- MedRAG/analyzeValidationResults.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("grid_search_results.csv")
-global_metrics = df.groupby(["Model", "Alpha"])[["Accuracy", "Precision", "Recall", "F1_Score"]].mean()
-global_metrics = global_metrics.sort_values(by="Accuracy", ascending=False)
-
-print("=== CLASSIFICA GLOBALE (Media dei 3 Dataset) ===\n")
-print(global_metrics)
